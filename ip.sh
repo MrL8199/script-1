e3ccf08ad9a5568d9e69ce8d84dd80b58ab06a87
evilbabylx <40205448+evilbabylx@users.noreply.github.com>
2019-09-20 13:37:26 +0800

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,41 +1,33 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# Path to your hosts file
-hostsFile="/etc/hosts"
+# PATH TO YOUR HOSTS FILE
+ETC_HOSTS=/etc/hosts
 
-# Default IP address for host
-ip="$(ping -c 1 gicungduoc.ddns.net | awk -F'[()]' '/PING/{print $2}')
+# DEFAULT IP FOR HOSTNAME
+IP=$(ping -c 1 gicungduoc.ddns.net | awk -F'[()]' '/PING/{print $2}')
 
 # Hostname to add/remove.
-hostname="$2"
-
-yell() { echo "$0: $*" >&2; }
-die() { yell "$*"; exit 111; }
-try() { "$@" || die "cannot $*"; }
-
-remove() {
-    if [ -n "$(grep -P "[[:space:]]$hostname" /etc/hosts)" ]; then
-        echo "$hostname found in $hostsFile. Removing now...";
-        try sudo sed -ie "/[[:space:]]$hostname/d" "$hostsFile";
+HOSTNAME="google.com"
+if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
+    then
+        echo "$HOSTNAME Found in your $ETC_HOSTS, Removing now...";
+        sudo sed -i".bak" "/$HOSTNAME/d" $ETC_HOSTS
     else
-        yell "$hostname was not found in $hostsFile";
+        echo "$HOSTNAME was not found in your $ETC_HOSTS";
     fi
-}
-
-add() {
-    if [ -n "$(grep -P "[[:space:]]$hostname" /etc/hosts)" ]; then
-        yell "$hostname, already exists: $(grep $hostname $hostsFile)";
-    else
-        echo "Adding $hostname to $hostsFile...";
-        try printf "%s\t%s\n" "$ip" "$hostname" | sudo tee -a "$hostsFile" > /dev/null;
-
-        if [ -n "$(grep $hostname /etc/hosts)" ]; then
-            echo "$hostname was added succesfully:";
-            echo "$(grep $hostname /etc/hosts)";
+HOSTS_LINE="$IP\t$HOSTNAME"
+    if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
+        then
+            echo "$HOSTNAME already exists : $(grep $HOSTNAME $ETC_HOSTS)"
         else
-            die "Failed to add $hostname";
-        fi
-    fi
-}
+            echo "Adding $HOSTNAME to your $ETC_HOSTS";
+            sudo -- sh -c -e "echo '$HOSTS_LINE' >> /etc/hosts";
 
-$@
+            if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
+                then
+                    echo "$HOSTNAME was added succesfully \n $(grep $HOSTNAME /etc/hosts)";
+                else
+                    echo "Failed to Add $HOSTNAME, Try again!";
+            fi
+    fi
+
